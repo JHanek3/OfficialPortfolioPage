b4bdd4ee3e98858ddcf77864c0de0f69a37c2d8f
Jon <>
2019-12-03 12:12:02 -0600

    Had to purge the whole contact sass and start over

---
+++ main.js
@@ -0,0 +1,27 @@
+function coPY(clicked_id) {
+  //copy the email
+  if (clicked_id === "bus") {
+    var copyText = "Hanekj25@gmail.com";
+    var dummy = document.createElement('input');
+    document.body.appendChild(dummy);
+    dummy.setAttribute("id", "dummy_id");
+    document.getElementById("dummy_id").value="Hanekj25@gmail.com";
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    alert("Copied to clipboard: " + copyText);
+  } else {
+    var copyText = "Simplecode4@gmail.com";
+    var dummy = document.createElement('input');
+    document.body.appendChild(dummy);
+    dummy.setAttribute("id", "dummy_id");
+    document.getElementById("dummy_id").value="Simplecode4@gmail.com";
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    alert("Copied to clipboard: " + copyText);
+  }
+}
+
+// style the rest of the page
+//carousel?
