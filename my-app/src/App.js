4ca9f7c389a0d21368ae2550f96b53232349b46b
Jon <>
2020-05-28 16:48:42 -0500

    added a staging area

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "./App.css"
-import Home from "./Components/Home"
 import List from "./Components/ToDoList"
 import MemeGenerator from "./Components/MemeGenerator"
 import {Link, Switch, Route} from "react-router-dom"
@@ -8,8 +7,11 @@ import {Link, Switch, Route} from "react-router-dom"
 function App() {
   return(
     <div>
+      <p>This is the project staging area, I haven't completly figured out how to anchor link a pure html file to work with a react app's Switch and Router hooks. It's probably just easier to make
+        this whole landing page into a react app, but that'll be after I finish some more projects.
+      </p>
+      <Link to="/List">ToDo List</Link> || <Link to="/Meme">Meme Generator</Link> || 
       <Switch>
-        <Route exact path="http://hanekpro.com/my-app/build/index.html"><Home/></Route>
         <Route exact path="/List"><List/></Route>
         <Route exact path="/Meme"><MemeGenerator/></Route>
       </Switch>
