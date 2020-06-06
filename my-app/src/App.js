874a9ab9a598c2e1fccac4fda1568b7c5cf3686f
Jon <>
2020-06-05 22:55:46 -0500

    Added some certs and links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./App.css"
 import List from "./Components/ToDoList"
 import MemeGenerator from "./Components/MemeGenerator"
+import Quote from "./Components/Quote"
 import {Link, Switch, Route} from "react-router-dom"
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
       <p>This is the project staging area, I haven't completly figured out how to anchor link a pure html file to work with a react app's Switch and Router hooks. It's probably just easier to make
         this whole landing page into a react app, but that'll be after I finish some more projects.
       </p>
-      <Link to="/List">ToDo List</Link> || <Link to="/Meme">Meme Generator</Link> || 
+      <Link to="/List">ToDo List</Link> || <Link to="/FunReads">A Quote a Day</Link> || <Link to="/Meme">Meme Generator</Link> || 
       <Switch>
         <Route exact path="/List"><List/></Route>
+        <Route exact path="/FunReads"><Quote/></Route>
         <Route exact path="/Meme"><MemeGenerator/></Route>
       </Switch>
     </div>
