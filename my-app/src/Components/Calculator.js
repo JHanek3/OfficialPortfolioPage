484eedae0b7e186ca58eb3f7afaae4239191b6f8
Jon <>
2020-07-20 19:32:51 -0500

    Fix the opacity?

--- my-app/src/Components/Calculator.js
+++ my-app/src/Components/Calculator.js
@@ -1,26 +1,32 @@
 import React, {useState}  from "react"
 
 function Calculator() {
-    const str = ""
-    
-    function adder(val) {
-        
-        return str.concat(val)
-    }
-    function solver(statement) {
-        return eval(statement)
-    }
     return (
         <div>
-            {str}
-                    <button
-                        onClick={adder(1)}>
-                    1</button>
-                    <button>+</button>
-                    <button type="submit" onSubmit={solver()}>=</button>
-
+            <p>sup?</p>
         </div>
     )
+
+    // const str = ""
+    
+    // function adder(val) {
+        
+    //     return str.concat(val)
+    // }
+    // function solver(statement) {
+    //     return eval(statement)
+    // }
+    // return (
+    //     <div>
+    //         {str}
+    //                 <button
+    //                     onClick={adder(1)}>
+    //                 1</button>
+    //                 <button>+</button>
+    //                 <button type="submit" onSubmit={solver()}>=</button>
+
+    //     </div>
+    // )
 }
 
 export default Calculator
