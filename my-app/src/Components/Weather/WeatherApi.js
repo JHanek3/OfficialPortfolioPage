085ca36b9c842bfe93b5ac9b08505e6c92eac6d0
Jon <>
2020-07-26 15:14:09 -0500

    gitignore

--- my-app/src/Components/Weather/WeatherApi.js
+++ my-app/src/Components/Weather/WeatherApi.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import "../../compCSS/Weather.css";
 import Daily from "./Daily"
 import axios from "axios"
-import API_KEY from "./key"
+import API_KEY from "../../../key"
 
 function WeatherAPI() {
   const [apiData, setApiData] = useState([])
--- my-app/src/Components/Weather/key.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = "6950dac4b2002e56ed04433eeb2a0abb"
-
-export default API_KEY
